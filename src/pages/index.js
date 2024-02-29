3cf0c1d885be3fa435cd83b82700864ee7339fad
Isabelle Löfgren <isabellefotograf@gmail.com>
2024-03-01 00:04:16 +0100

    added som tailwind styling to index.js and added data.js with questions to quiz.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,20 @@ export default function Home() {
     <>
       <main>
         <h1>Home</h1>
-        <Link href="/admin">admin</Link>
+        <Link href="/admintest">admin</Link>
+        <div className="container mx-auto relative max-w-5xl pt-20 sm:pt-24 lg:pt-32 px-4">
+          <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center">
+            Welcome to our Quiz!
+          </h1>
+          <div className="mt-6 sm:mt-10 flex justify-center space-x-6 text-sm">
+            <button className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto">
+              <Link href="/quiz">Start here</Link>
+            </button>
+            <button className=" hover:bg-slate-700 outline-dotted focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-black font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto">
+              <Link href="/admintest">Create your own quiz!</Link>
+            </button>
+          </div>
+        </div>
       </main>
     </>
   );
---
+++ src/pages/quiz.js
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import { quiz } from "../data.js";
+
+const page = () => {
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [checked, setChecked] = useState(false);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
+
+  const { questions } = quiz;
+  const { question, answers, correctAnswer } = questions[activeQuestion];
+
+  //   Select and check answer
+  const onAnswerSelected = (answer, idx) => {
+    setChecked(true);
+    setSelectedAnswerIndex(idx);
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true);
+      console.log("true");
+    } else {
+      setSelectedAnswer(false);
+      console.log("false");
+    }
+  };
+
+  // Calculate score and increment to next question
+  const nextQuestion = () => {
+    setSelectedAnswerIndex(null);
+    setResult((prev) =>
+      selectedAnswer
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : {
+            ...prev,
+            wrongAnswers: prev.wrongAnswers + 1,
+          }
+    );
+    if (activeQuestion !== questions.length - 1) {
+      setActiveQuestion((prev) => prev + 1);
+    } else {
+      setActiveQuestion(0);
+      setShowResult(true);
+    }
+    setChecked(false);
+  };
+
+  return (
+    <div className="container mx-auto relative max-w-5xl pt-20 sm:pt-24 lg:pt-32 px-4">
+      <h1>Quiz Page</h1>
+      <div>
+        <h2>
+          Question: {activeQuestion + 1}
+          <span>/{questions.length}</span>
+        </h2>
+      </div>
+      <div>
+        {!showResult ? (
+          <div className="">
+            <h3>{questions[activeQuestion].question}</h3>
+            {answers.map((answer, idx) => (
+              <li
+                key={idx}
+                onClick={() => onAnswerSelected(answer, idx)}
+                className={
+                  selectedAnswerIndex === idx ? "li-selected" : "li-hover"
+                }
+              >
+                <span>{answer}</span>
+              </li>
+            ))}
+            {checked ? (
+              <button onClick={nextQuestion} className="btn">
+                {activeQuestion === question.length - 1 ? "Finish" : "Next"}
+              </button>
+            ) : (
+              <button onClick={nextQuestion} disabled className="btn-disabled">
+                {" "}
+                {activeQuestion === question.length - 1 ? "Finish" : "Next"}
+              </button>
+            )}
+          </div>
+        ) : (
+          <div className="">
+            <h3>Results</h3>
+            <h3>Overall {(result.score / 25) * 100}%</h3>
+            <p>
+              Total Questions: <span>{questions.length}</span>
+            </p>
+            <p>
+              Total Score: <span>{result.score}</span>
+            </p>
+            <p>
+              Correct Answers: <span>{result.correctAnswers}</span>
+            </p>
+            <p>
+              Wrong Answers: <span>{result.wrongAnswers}</span>
+            </p>
+            <button
+              className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto"
+              onClick={() => window.location.reload()}
+            >
+              Next
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default page;
